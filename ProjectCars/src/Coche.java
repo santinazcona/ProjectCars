d3e1e3e40f8d04c781e8ad8e43716c7542899890
Julen <Julen@10.164.54.93>
2017-11-03 09:50:18 +0100

    post add index-conflict fix

--- ProjectCars/src/Coche.java
+++ ProjectCars/src/Coche.java
@@ -1,11 +1,10 @@
 
 public class Coche {
-<<<<<<< HEAD
 	
 	public static void main(String[] args){
 		
 	}
-=======
+
 	//Prueba
->>>>>>> branch 'master' of https://github.com/santinazcona/ProjectCars.git
+
 }
