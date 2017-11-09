32b0e2ca0d647d0a2c34d2d488340e0866fc0e14
UnaiJaure <Unai Jauregibeitia@opendeusto.es>
2017-11-09 09:27:51 +0100

    Base coche

--- ProjectCars/src/Vehiculo/Coche.java
+++ ProjectCars/src/Vehiculo/Coche.java
@@ -2,9 +2,42 @@ package Vehiculo;
 
 public class Coche {
 	
-	public static void main(String[] args){
-		
+	protected String name;
+	protected double posX;
+	protected double posY;
+	
+	// Constructor vacio
+	public Coche(){
+		name = "";
+		posX = 0;
+		posY = 0;
 	}
+	// Constructor base 1
+	public Coche(String name, double posX, double posY){
+		this.name = name;
+		this.posX = posX;
+		this.posY = posY;
+	}
+	
+	//Getters and Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPosX() {
+		return posX;
+	}
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+	public double getPosY() {
+		return posY;
+	}
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+	
 
-	//Prueba2
 }
