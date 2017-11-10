9d333b24dbd942c5fc048a5297f9637b3d965841
Julen <Julen@10.164.48.136>
2017-11-10 09:34:29 +0100

    bd primera prueba

---
+++ ProjectCars/src/BaseDeDatos/BD.java
@@ -0,0 +1,46 @@
+package BaseDeDatos;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Vehiculo.Coche;
+
+public class BD {
+	private Connection conexion;
+	private static Statement stmt;
+	
+	
+		public void conectar(){
+			try{
+				conexion = DriverManager.getConnection("jdbc:sqlite:ProjectCars.db");
+		    } catch (SQLException E) {
+		        E.printStackTrace();
+		    	throw new RuntimeException("No se ha podido realizar la conexion");
+		        
+		    }
+		}
+		public void desconectar(){
+			try{
+				conexion.close();
+			}catch(SQLException E){
+				E.printStackTrace();
+			}
+		}
+		public void Sentencia(){
+			try{
+				stmt = conexion.createStatement();
+			}catch(SQLException E){
+				E.printStackTrace();
+			}
+		}
+		
+		public void closeSentencia(){
+			try{
+				stmt.close();
+			}catch(SQLException E){
+				E.printStackTrace();
+			}
+		} 
+}
